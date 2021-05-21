d3de074b9152a25ccf4c88594dc26419e046de1a
QuinntyneBrown <quinntynebrown@gmail.com>
2021-05-21 00:04:58 -0400

    refactor

--- src/Clarity.Core/Models/Board.cs
+++ src/Clarity.Core/Models/Board.cs
@@ -8,11 +8,11 @@ namespace Clarity.Core.Models
     {
         public Guid BoardId { get; private set; }
         public string Name { get; private set; }
-        public ICollection<BoardState> BoardStates { get; private set; } = new HashSet<BoardState>();
+        public List<BoardState> BoardStates { get; private set; } = new ();
         public static Board WithDefaults(string name)
         {
             int order = 1;
-            Board board = new() { Name = name };
+            Board board = new(name);
 
             foreach (var state in Enum.GetValues<StateType>().Select(x => new BoardState(x, order++, board.BoardId)).ToList())
             {
@@ -27,9 +27,6 @@ namespace Clarity.Core.Models
             Name = name;
         }
 
-        private Board()
-        {
-
-        }
+        private Board() { }
     }
 }
--- src/Clarity.Core/Models/Ticket.cs
+++ src/Clarity.Core/Models/Ticket.cs
@@ -21,9 +21,9 @@ namespace Clarity.Core.Models
         public int Effort { get; private set; }
         public int Priority { get; private set; }
         public TeamMember TeamMember { get; private set; }
-        public ICollection<TicketState> TicketStates { get; private set; } = new HashSet<TicketState>();
+        public List<TicketState> TicketStates { get; private set; } = new ();
         public TicketState CurrentTicketState { get => TicketStates.OrderByDescending(x => x.Created).First(); }
-        public ICollection<Comment> Comments { get; private set; } = new HashSet<Comment>();
+        public List<Comment> Comments { get; private set; } = new ();
         public List<TicketEffortChanged> EffortChangedEvents { get; private set; } = new();
 
         public Ticket(Guid teamMemberId, string name, string url, Html acceptanceCriteria, Html description)
