15721c9302ee4e549bcefde04e364480ac8be377
QuinntyneBrown <quinntynebrown@gmail.com>
2021-05-24 21:32:08 -0400

    feat(pr)

---
+++ src/Clarity.Core/Models/PullRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Clarity.Core.Models
+{
+    public class PullRequest
+    {
+        public Guid PullRequestId { get; private set; }
+        public string Url { get; private set; }
+        public string TicketName { get; private set; }
+    }
+}
