a10ef4a659d34a91f9e72a5baf96eb4a1ec41047
Quinntyne Brown <quinntynebrown@gmail.com>
2023-03-11 23:57:20 -0500

    update

--- src/Services/Clarity/Clarity.Domain/Features/BoardStates/StateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Clarity.Domain.Features
-{
-    public class StateDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string DisplayName { get; set; }
-    }
-}
