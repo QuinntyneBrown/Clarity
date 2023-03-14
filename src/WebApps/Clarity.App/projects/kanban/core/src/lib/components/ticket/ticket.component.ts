50a42ccc77e78b9c0345bd2e9e095d130647ebaa
Quinntyne Brown <quinntynebrown@gmail.com>
2023-03-13 23:10:32 -0400

    update

--- src/WebApps/Clarity.App/projects/kanban/core/src/lib/components/ticket/ticket.component.ts
+++ src/WebApps/Clarity.App/projects/kanban/core/src/lib/components/ticket/ticket.component.ts
@@ -1,22 +1,39 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { createTicketViewModel } from './create-ticket-view-model';
 import { PushModule } from '@ngrx/component';
 import { Ticket } from '../../models';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { UpsertTicketComponent } from '../upsert-ticket';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, PushModule],
+  imports: [CommonModule, PushModule, DialogModule],
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent {
   public vm$ = createTicketViewModel();
 
+  private readonly _dialog = inject(Dialog)
+
   @Input() ticket!:Ticket;
+
+  public handleEditClick() {
+    this._dialog.open(UpsertTicketComponent, {
+
+    }).closed
+    .subscribe();
+  }
+
+  constructor(
+    
+  ) {
+
+  }
 }
--- src/WebApps/Clarity.App/projects/kanban/core/src/lib/components/upsert-ticket/upsert-ticket.component.ts
+++ src/WebApps/Clarity.App/projects/kanban/core/src/lib/components/upsert-ticket/upsert-ticket.component.ts
@@ -1,19 +1,22 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { createUpsertTicketViewModel } from './create-upsert-ticket-view-model';
 import { PushModule } from '@ngrx/component';
+import { DialogModule, DialogRef } from '@angular/cdk/dialog';
 
 @Component({
-  selector: 'app-upsert-ticket',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, PushModule],
+  imports: [CommonModule, PushModule, DialogModule],
   templateUrl: './upsert-ticket.component.html',
   styleUrls: ['./upsert-ticket.component.scss']
 })
 export class UpsertTicketComponent {
+
+  private _dialogRef = inject(DialogRef);
+
   public vm$ = createUpsertTicketViewModel();
 }
